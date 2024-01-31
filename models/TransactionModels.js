da2e9455b1e15f70132692b9014b633f8da1c4e2
Maria Vitória <mariavivibibi120@gmail.com>
2024-01-31 15:06:56 -0300

    inicio da API

---
+++ models/TransactionModels.js
@@ -0,0 +1,9 @@
+class Transaction {
+    constructor(id, description, amount, date) {
+    this.id = id;
+    this.description = description;
+    this.amount = amount;
+    this.date = date;
+    }
+}
+
